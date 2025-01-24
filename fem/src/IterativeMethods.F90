5d7d67c56d13788cb22d0ab2f453df98d5968624
Juha Ruokolainen <jpr@keisarikotka.lnx.csc.fi>
2025-01-24 11:29:40 +0200

    [skip ci]

--- fem/src/IterativeMethods.F90
+++ fem/src/IterativeMethods.F90
@@ -762,6 +762,7 @@ CONTAINS
       bnrm  = normfun(n, b(1), 1 )
       rnrm0 = normfun(n, work(1,r), 1 )
       rnrm0 = sqrt(sum(work(1:n,r)**2))
+      rnrm0 = normfun(n, work(1:n,r), 1 )
 
       !-------------------------------------------------------------------
       ! Check whether the initial guess is already converged, diverged or NaN
